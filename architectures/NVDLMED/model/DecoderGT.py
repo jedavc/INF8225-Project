b466bf3bb1a325d1b5d637d28ea33bccc1ec026b
jedavc <jeremie.david@polymtl.ca>
2020-03-31 02:30:12 -0400

    avancement de la partie DecodeurGT

--- architectures/NVDLMED/model/DecoderGT.py
+++ architectures/NVDLMED/model/DecoderGT.py
@@ -2,10 +2,11 @@ from torch import nn
 
 
 class DecoderGT(nn.Module):
-    def __init__(self, input_shape=(4, 160, 192, 128)):
+    def __init__(self):
         super(DecoderGT, self).__init__()
 
-        self.input_shape = input_shape
+    def forward(self, x1, x2, x3, x4):
 
-    def forward(self, x):
-        
+        # First ResNetBlock (output filters = 128)
+        x = nn.Conv3d(in_channels=x4.shape[1], out_channels=128, kernel_size=(1, 1, 1), stride=1)(x4)
+        x = nn.Upsample(size=2*x.shape)
