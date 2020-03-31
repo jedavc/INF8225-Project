6ce82cfbea0ae8db61bab68bd46770de7f0880e7
jedavc <jeremie.david@polymtl.ca>
2020-03-31 15:20:55 -0400

    class DecoderGT treminé

--- architectures/NVDLMED/model/Encoder.py
+++ architectures/NVDLMED/model/Encoder.py
@@ -14,21 +14,21 @@ class Encoder(nn.Module):
         x = nn.Conv3d(in_channels=self.input_shape[0], out_channels=32, kernel_size=(3, 3, 3), stride=1)(x)
         x = nn.Dropout3d(p=0.2)(x)
 
-        # First ResNetBlock (output filters = 32)
+        # First Encoder ResNetBlock (output filters = 32)
         x1 = ResNetBlock(in_channel=32, out_channel=32)(x)
         x = nn.Conv3d(in_channels=32, out_channels=32, kernel_size=(3, 3, 3), stride=2)(x1)
 
-        # Second ResNetBlock (output filters = 64)
+        # Second Encoder ResNetBlock (output filters = 64)
         x = ResNetBlock(in_channel=32, out_channel=64)(x)
         x2 = ResNetBlock(in_channel=64, out_channel=64)(x)
         x = nn.Conv3d(in_channels=64, out_channels=64, kernel_size=(3, 3, 3), stride=2)(x2)
 
-        # Third ResNetBlock (output filters = 128)
+        # Third Encoder ResNetBlock (output filters = 128)
         x = ResNetBlock(in_channel=64, out_channel=128)(x)
         x3 = ResNetBlock(in_channel=128, out_channel=128)(x)
         x = nn.Conv3d(in_channels=128, out_channels=128, kernel_size=(3, 3, 3), stride=2)(x3)
 
-        # Fourth ResNetBlock (output filters = 256)
+        # Fourth Encoder ResNetBlock (output filters = 256)
         x = ResNetBlock(in_channel=128, out_channel=256)(x)
         x = ResNetBlock(in_channel=256, out_channel=256)(x)
         x = ResNetBlock(in_channel=256, out_channel=256)(x)
